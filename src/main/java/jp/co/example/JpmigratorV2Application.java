63c946416af8497018cad9c9a1045f4578d3de10
Hagiwara Shuhei <shuhei.hagiwara@FLECT-IS177.local>
2017-04-18 18:05:56 +0900

    initial commit

---
+++ src/main/java/jp/co/example/JpmigratorV2Application.java
@@ -0,0 +1,12 @@
+package jp.co.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmigratorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpmigratorV2Application.class, args);
+	}
+}
